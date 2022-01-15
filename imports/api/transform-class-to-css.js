04b3cc09a22e885e1ac58127a4fff66700786fe4
Gustavo Kich <gh.kich@gmail.com>
2022-01-15 13:59:24 -0300

    Add more Tailwind classes

--- imports/api/transform-class-to-css.js
+++ imports/api/transform-class-to-css.js
@@ -9,6 +9,12 @@ const spacing = {
   3.5: '0.875rem',
   4: '1rem',
   5: '1.25rem',
+  6: '1.5rem',
+  7: '1.75rem',
+  8: '2rem',
+  10: '2.5rem',
+  12: '3rem',
+  16: '4rem',
   64: '16rem',
   80: '20rem',
 }
@@ -17,6 +23,7 @@ const heights = Object.entries(spacing).reduce((newObj, [key, val]) => {
   const spacingHeights = Object.assign(newObj, {[`h-${key}`]: `height: ${val};`})
   return {
     ...spacingHeights,
+    'h-auto': 'height: auto;',
     'h-full': 'height: 100%;',
     'h-screen': 'height: 100vh;',
   }
@@ -26,6 +33,7 @@ const widths = Object.entries(spacing).reduce((newObj, [key, val]) => {
   const spacingWidths = Object.assign(newObj, {[`w-${key}`]: `width: ${val};`})
   return {
     ...spacingWidths,
+    'w-auto': 'width: auto',
     'w-full': 'width: 100%;',
     'w-screen': 'width: 100vw;',
   }
@@ -66,6 +74,7 @@ const margins = Object.entries(spacing).reduce((newObj, [key, val]) => {
     ...marginsRight,
     ...marginsTop,
     ...marginsBottom,
+    'mx-auto': 'margin: 0 auto;',
   }
 }, {})
 
@@ -75,27 +84,69 @@ export const transformClassToCss = (className, theme) => {
     hidden: `display: none;`,
     block: `display: block;`,
     flex: `display: flex;`,
+    relative: 'position: relative;',
+    absolute: 'position: absolute;',
     ...heights,
     ...widths,
+    'max-w-xs': 'max-width: 20rem;',
+    'max-w-sm': 'max-width: 24rem;',
+    'max-w-md': 'max-width: 28rem;',
     ...paddings,
     ...margins,
+    'appearance-none': 'appearance: none;',
     'flex-col': `flex-direction: column;`,
     'justify-center': `justify-content: center;`,
+    'justify-between': `justify-content: space-between;`,
     'items-center': `align-items: center;`,
+    'items-baseline': `align-items: baseline;`,
     'bg-primary': `background-color: ${settings?.colors?.primary};`,
     'bg-secondary': `background-color: ${settings?.colors?.secondary};`,
     'bg-white': `background-color: white;`,
+    'bg-black': `background-color: black;`,
+    'bg-gray-800': `background-color: #1f2937;`,
+    'bg-gray': `background-color: #111827;`,
+    'placeholder-gray': `background-color: #d1d5db;`,
     'border-white': `border-color: white;`,
-    'border-gray': `border-color: gray;`,
+    'border-gray': `border-color: #d1d5db;`,
     'text-primary': `color: ${settings?.colors?.primary};`,
     'text-secondary': `color: ${settings?.colors?.secondary};`,
     'text-white': `color: white;`,
-    'text-sm': `font-size: 12px;`,
-    'shadow-lg': `box-shadow: 0 2px 10px rgba(0,0,0,0.2);`,
-    'shadow-xl': `box-shadow: 0 5px 20px rgba(0,0,0,0.2);`,
+    'text-gray-300': `color: #d1d5db;`,
+    'text-gray-400': `color: #9ca3af;`,
+    'text-gray': `color: #111827;`,
+    'text-gray-900': `color: #111827;`,
+    'text-xs': `font-size: 0.75rem; line-height: 1rem;`,
+    'text-sm': `font-size: 0.875rem; line-height: 1.25rem;`,
+    'text-base': `font-size: 1rem; line-height: 1.5rem;`,
+    'text-lg': `font-size: 1.125rem; line-height: 1.75rem;`,
+    'text-xl': `font-size: 1.25rem; line-height: 1.75rem;`,
+    'text-2xl': `font-size: 1.5rem; line-height: 2rem;`,
+    'text-3xl': `font-size: 1.875rem; line-height: 2.25rem;`,
+    'text-center': `text-align: center;`,
+    'font-light': `font-weight: 300;`,
+    'font-normal': `font-weight: 400;`,
+    'font-medium': `font-weight: 500;`,
+    'font-semibold': `font-weight: 600;`,
+    'font-bold': `font-weight: 700;`,
+    'font-extrabold': `font-weight: 800;`,
+    'shadow-sm': `box-shadow: 0 1px 2px 0 rgb(0 0 0 / 0.05);`,
+    shadow: `box-shadow: 0 1px 3px 0 rgb(0 0 0 / 0.1), 0 1px 2px -1px rgb(0 0 0 / 0.1);`,
+    'shadow-md': `box-shadow: 0 4px 6px -1px rgb(0 0 0 / 0.1), 0 2px 4px -2px rgb(0 0 0 / 0.1);`,
+    'shadow-lg': `box-shadow: 0 10px 15px -3px rgb(0 0 0 / 0.1), 0 4px 6px -4px rgb(0 0 0 / 0.1);`,
+    'shadow-xl': `box-shadow: 0 20px 25px -5px rgb(0 0 0 / 0.1), 0 8px 10px -6px rgb(0 0 0 / 0.1);`,
     'cursor-pointer': `cursor: pointer;`,
     border: `border-style: solid; border-width: 1px;`,
-    rounded: `border-radius: 6px;`,
+    'rounded-none': `border-radius: 0px;`,
+    'rounded-sm': `border-radius: ${settings.roundLevel * 0.125}rem;`,
+    rounded: `border-radius: ${settings.roundLevel * 0.25}rem;`,
+    'rounded-md': `border-radius: ${settings.roundLevel * 0.375}rem;`,
+    'rounded-full': `border-radius: 50%;`,
+    'rounded-t-md': `border-top-left-radius: ${settings.roundLevel * 0.375}rem; border-top-right-radius: ${
+      settings.roundLevel * 0.375
+    }rem;`,
+    'rounded-b-md': `border-bottom-left-radius: ${settings.roundLevel * 0.375}rem; border-bottom-right-radius: ${
+      settings.roundLevel * 0.375
+    }rem;`,
   }
 
   return obj[className] ? obj[className] : ``
