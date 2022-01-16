9a1c75f4c42babd4b75408e50ed96b72a5e86886
Gustavo Kich <gh.kich@gmail.com>
2022-01-16 19:53:41 -0300

    Update Lenna script

--- _lenna/Lenna.js
+++ _lenna/Lenna.js
@@ -2,20 +2,26 @@ import {useEffect, useState} from 'react'
 import {useRouter} from 'next/router'
 
 export const Lenna = ({appId = '', children}) => {
-  const [iframePathname, setIframePathname] = useState(`/apps/${appId}`)
+  const [iframePathname, setIframePathname] = useState(appId ? `/apps/${appId}` : ``)
+  const [sidebarMinimized, setSidebarMinimized] = useState(false)
   const router = useRouter()
 
   useEffect(() => {
+    sendHtmlAndLoadCss()
     const handlePostMessage = (e) => {
-      if (e.data === 'toggleView') {
-        const lennaElement = document.getElementById('__lenna')
-        lennaElement.classList.toggle('full-view')
+      const lennaElement = document.getElementById('__lenna')
+      if (e.data.message === 'toggleView') {
+        if (e.data.value === 'full') {
+          lennaElement.classList.add('full-view')
+        } else {
+          sendHtmlAndLoadCss()
+          lennaElement.classList.remove('full-view')
+        }
       }
 
       if (e.data.message === 'pageAccess') {
-        if (e.data.existingPage) {
-          router.replace(e.data.pagePath)
-        }
+        sendHtmlAndLoadCss()
+        router.replace(e.data.pagePath)
       }
     }
     window.addEventListener('message', handlePostMessage)
@@ -26,11 +32,13 @@ export const Lenna = ({appId = '', children}) => {
   }, [])
 
   const lennaApiCommunication = () => {
-    const contentElement = document.getElementById('__lenna-content')
+    const contentElement = document.querySelector('#__lenna .lenna-content')
     const pageHTML = contentElement?.innerHTML
-    if (window.pageHtml !== pageHTML) {
-      window.pageHtml = pageHTML
+    const stringifiedPageHTML = JSON.stringify(pageHTML)
+    if (window.pageHtml !== stringifiedPageHTML) {
+      window.pageHtml = stringifiedPageHTML
       const xhr = new XMLHttpRequest()
+      console.log('sending-data')
       xhr.open('POST', 'http://localhost:3050/api/sendHtml', true)
       xhr.setRequestHeader('Content-Type', 'application/json; charset=UTF-8')
       xhr.send(
@@ -42,7 +50,7 @@ export const Lenna = ({appId = '', children}) => {
       )
       xhr.onloadend = function () {
         // done
-        console.log('dataSent')
+        console.log('data-sent')
       }
 
       xhr.open('POST', 'http://localhost:3050/api/getCss', true)
@@ -76,19 +84,19 @@ export const Lenna = ({appId = '', children}) => {
 
   const sendHtmlAndLoadCss = () => {
     lennaApiCommunication()
-    setInterval(() => {
-      lennaApiCommunication()
-    }, 1000)
+  }
+
+  const toggleSidebar = () => {
+    setSidebarMinimized((prev) => !prev)
   }
 
   return (
     <>
       <style jsx>{`
-        #__lenna > div {
-          pointer-events: none;
+        #__lenna .lenna-content {
           transition: opacity 0.2s linear;
           position: fixed;
-          right: 320px;
+          right: 327px;
           left: 0;
           height: 100vh;
         }
@@ -102,15 +110,49 @@ export const Lenna = ({appId = '', children}) => {
           height: 100vh;
           border: none;
           z-index: 10;
+          transition: transform 0.15s linear;
+          background-color: #f9f9f9;
+          border-left: 1px solid rgb(228, 228, 231);
         }
 
         #__lenna.full-view > iframe {
           width: 100vw;
         }
+
+        #__lenna .lenna-sidebar-toggle {
+          width: 6px;
+          height: 100vh;
+          background-color: rgb(244, 244, 245);
+          border-left: 1px solid rgb(228, 228, 231);
+          position: fixed;
+          top: 0;
+          right: 320px;
+          bottom: 0;
+          z-index: 15;
+          cursor: pointer;
+          transition: right 0.15s linear;
+        }
+
+        #__lenna .lenna-sidebar-toggle:hover {
+          background-color: rgb(235, 235, 236);
+        }
+
+        #__lenna.sidebar-minimized .lenna-content {
+          right: 0;
+        }
+
+        #__lenna.sidebar-minimized > iframe {
+          transform: translateX(320px);
+        }
+
+        #__lenna.sidebar-minimized .lenna-sidebar-toggle {
+          right: 0;
+        }
       `}</style>
-      <div id="__lenna" onLoad={sendHtmlAndLoadCss}>
-        <iframe id="__lenna-app" src={`http://localhost:3050${iframePathname}`} allow="clipboard-write" />
-        <div id="__lenna-content" style={{opacity: 0}}>
+      <div id="__lenna" className={sidebarMinimized ? 'sidebar-minimized' : ''}>
+        <iframe src={`http://localhost:3050${iframePathname}`} allow="clipboard-write" />
+        <div className="lenna-sidebar-toggle" onClick={toggleSidebar} />
+        <div className="lenna-content" style={{opacity: 0}}>
           {children}
         </div>
       </div>
