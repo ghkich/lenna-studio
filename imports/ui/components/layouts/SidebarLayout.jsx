298446b08899c310f619ca3f1326f3dd4bb53147
gustavo <gh.kich@gmail.com>
2022-03-09 16:35:00 -0700

    Improve responsiveness

--- imports/ui/components/layouts/SidebarLayout.jsx
+++ imports/ui/components/layouts/SidebarLayout.jsx
@@ -58,9 +58,11 @@ export const SidebarLayout = ({children, contentComponent, loading, menuMinimize
 
   return (
     <div data-ls="AuthenticatedLayout" className="flex">
-      <div className="content flex-1 hidden sm:block bg-white">{contentComponent}</div>
+      <div className="content flex-1 hidden sm:block bg-gradient-to-bl from-blue-500 to-purple-500">
+        {contentComponent}
+      </div>
       <div className="w-1.5 h-screen bg-gray-100 hover:bg-gray-200 border-l border-gray-200 hover:bg-opacity-75 cursor-pointer hidden sm:block" />
-      <div className="sidebar flex flex-col bg-white text-gray-500 text-xs w-full sm:w-80 max-w-xs h-screen sm:border-l border-gray-200">
+      <div className="sidebar flex flex-col bg-white text-gray-500 text-xs w-full sm:w-80 h-screen sm:border-l border-gray-200">
         {user && (
           <>
             <div className="bg-white border-b h-7 px-1 flex justify-between items-center">
@@ -104,7 +106,7 @@ export const SidebarLayout = ({children, contentComponent, loading, menuMinimize
             )}
           </>
         )}
-        <div className="flex-1 h-screen p-4 overflow-auto relative">
+        <div className={`flex-1 h-screen ${user ? 'p-4' : 'px-4'} overflow-auto relative`}>
           {loading && (
             <div className="absolute top-0 right-0 bottom-0 left-0 bg-white flex justify-center">
               <FontAwesomeIcon icon={faLoader} className="text-xl my-10" spin />
