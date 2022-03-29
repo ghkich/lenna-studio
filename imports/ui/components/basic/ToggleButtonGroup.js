2ff8f0e893cd005dd15a0692fcac35547832a48f
gustavo <gh.kich@gmail.com>
2022-03-29 12:41:17 -0600

    Improve ToggleButtonGroup style

--- imports/ui/components/basic/ToggleButtonGroup.js
+++ imports/ui/components/basic/ToggleButtonGroup.js
@@ -7,9 +7,9 @@ export const ToggleButtonGroup = ({buttons, activeButton, onToggle}) => {
         <button
           key={button.value}
           type="button"
-          className={`flex-1 p-1.5 border ${activeButton === button.value ? 'bg-blue-50 text-blue-500' : ''} ${
-            index + 1 < buttons.length ? 'border-r-0 rounded-l-sm' : 'rounded-r-sm'
-          }`}
+          className={`flex-1 p-1.5 border font-bold ${
+            activeButton === button.value ? 'bg-blue-50 text-blue-500' : ''
+          } ${index + 1 < buttons.length ? 'border-r-0 rounded-l-sm' : 'rounded-r-sm'}`}
           onClick={() => onToggle(button.value)}
         >
           {button.label}
