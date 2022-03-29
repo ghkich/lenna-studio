fcb93b04d896781b725ec5ac962f6f5e9153ec23
gustavo <gh.kich@gmail.com>
2022-03-29 12:23:43 -0600

    Improve Button hover style

--- imports/ui/components/basic/Button.js
+++ imports/ui/components/basic/Button.js
@@ -6,7 +6,7 @@ export const Button = ({style, className, children, ...props}) => {
       type="button"
       className={`${
         style === 'primary'
-          ? 'bg-blue-500 hover:bg-blue-400 text-white font-semibold'
+          ? 'bg-blue-500 hover:bg-opacity-90 text-white font-semibold'
           : 'bg-white text-gray-600 hover:bg-gray-50 border'
       } p-2 rounded-sm  transition-colors duration-200 uppercase tracking-wider ${className}`}
       {...props}
