107bd932a5163cf6b370bd601a7412042e97b1c9
gustavo <gh.kich@gmail.com>
2022-03-29 12:44:10 -0600

    Add PulseButton component

---
+++ imports/ui/pages/home/components/PulseButton.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import {Button} from '../../../components/basic/Button'
+
+export const PulseButton = ({className, children, ...props}) => {
+  return (
+    <div className={`relative ${className}`}>
+      <Button {...props} className={`w-full relative z-10`}>
+        {children}
+      </Button>
+      <div className="absolute top-0.5 left-0 right-0 z-0 flex justify-center">
+        <div className="animate-ping w-full h-7 bg-gradient-to-r from-transparent via-blue-500 to-transparent rounded opacity-100" />
+      </div>
+    </div>
+  )
+}
