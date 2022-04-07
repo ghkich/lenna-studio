b1210e40c413a2a5ed7c825d18530cba1800784e
gustavo <gh.kich@gmail.com>
2022-04-06 20:31:26 -0700

    Allow Select to be disabled

--- imports/ui/components/basic/Select.js
+++ imports/ui/components/basic/Select.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import {useFormContext} from 'react-hook-form'
 
-export const Select = ({name, label, defaultValue, value, onChange, className, options}) => {
+export const Select = ({name, label, defaultValue, value, onChange, className, options, ...props}) => {
   const {register} = useFormContext() || {}
 
   return (
@@ -13,6 +13,7 @@ export const Select = ({name, label, defaultValue, value, onChange, className, o
         className="px-1.5 py-2 font-semibold border border-gray-300 required:bg-gray-100 w-full"
         {...(register && name ? register(name) : {})}
         onChange={onChange}
+        {...props}
       >
         {options?.map((option) => (
           <option key={option.value} value={option.value}>
